79f5a07b795fbac44db5db22459b7011ea52f4cb
Vitaliy-126 <vitalik.vinnik.05@gmail.com>
2021-03-18 23:00:37 +0200

    added Lab 3

---
+++ Lab 3/MyLibrary/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace MyLibrary
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+            size = 0;
+        }
+        public MyClass(bool[] array)
+        {
+            this.array = array;
+            size = array.Length;
+        }
+        public bool this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < size)
+                {
+                    if (array[index] == true)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    throw new Exception("index out of range");
+                }
+            }
+        }
+
+        public bool Result
+        {
+            get
+            {
+                if (size > 0)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i] == true)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    throw new Exception("array is empty");
+                }
+            }
+        }
+
+        private bool[]array;
+        private int size;
+    }
+}
---
+++ Lab 3/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyLibrary;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            bool[] array = { true, false, false };
+            MyClass my=new MyClass();
+            Console.WriteLine(my.Result);
+            Console.ReadKey();
+        }
+    }
+}
