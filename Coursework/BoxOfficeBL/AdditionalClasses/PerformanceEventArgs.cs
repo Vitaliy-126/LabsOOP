ec986674b2d4663bf7f3ce48512e039596891564
Vitaliy <vitalik.vinnik.05@gmail.com>
2021-05-11 21:46:02 +0300

    added PerformanceEventArgs

---
+++ Coursework/BoxOfficeBL/AdditionalClasses/PerformanceEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BoxOfficeBL.Model;
+
+namespace BoxOfficeBL.AdditionalClasses
+{
+    public delegate void PerformanceHandler(object sender, PerformanceEventArgs args);
+    public class PerformanceEventArgs
+    {
+        public Performance Performance { get; }
+        public DateTime Date { get; }
+        public PerformanceEventArgs(Performance performance, DateTime date)
+        {
+            Performance = performance;
+            Date = date.Date;
+        }
+    }
+}
