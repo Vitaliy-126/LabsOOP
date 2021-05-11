74e218b3c8fbc35f067bed565be6476e29172389
Vitaliy <vitalik.vinnik.05@gmail.com>
2021-05-11 23:31:10 +0300

    added TicketEventArgs

---
+++ Coursework/BoxOfficeBL/AdditionalClasses/TicketEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using BoxOfficeBL.Model;
+
+namespace BoxOfficeBL.AdditionalClasses
+{
+    public delegate void TicketHandler(object sender, TicketEventArgs args);
+    public class TicketEventArgs
+    {
+        public Ticket Ticket { get; }
+        public string CommunicationMedium { get; }
+        public TicketEventArgs(Ticket ticket, string communicationMedium)
+        {
+            Ticket = ticket;
+            CommunicationMedium = communicationMedium;
+        }
+    }
+}
