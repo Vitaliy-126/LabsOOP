6cb5795057a46c4f28a1f99fd5afe395f43cd263
Vitaliy <vitalik.vinnik.05@gmail.com>
2021-05-15 00:54:59 +0300

    added changes

--- Coursework/BoxOfficeCMD/Program.cs
+++ Coursework/BoxOfficeCMD/Program.cs
@@ -14,11 +14,11 @@ namespace BoxOfficeCMD
             boxOffice.PerformanceAdded += (object source, PerformanceEventArgs arg) => { posterPerformances.AddPoster(new Poster(arg.Performance, arg.Date)); };
             boxOffice.PerformanceDeleted += (object source, PerformanceEventArgs arg) => { posterPerformances.DeletePoster(new Poster(arg.Performance, arg.Date)); };
             boxOffice.TicketSold += AdditionalFunctions.SendingTicket;
-            boxOffice.AddInShow(new Performance("Тартюф", "Мольер", "Комедия", 1664), new Hall("Главный зал", 10, 10), Convert.ToDateTime("12-05-2021 19:40"), 100);
-            boxOffice.AddInShow(new Performance("Тартюф", "Мольер", "Комедия", 1664), new Hall("Главный зал", 10, 10), Convert.ToDateTime("12-05-2021 18:40"), 150);
-            boxOffice.AddInShow(new Performance("Укрощение строптивой", "Уильям Шекспир", "Комедия", 1954), new Hall("Главный зал", 10, 10), Convert.ToDateTime("12-05-2021 20:40"), 120);
-            boxOffice.AddInShow(new Performance("Укрощение строптивой", "Уильям Шекспир", "Комедия", 1954), new Hall("Главный зал", 10, 10), Convert.ToDateTime("12-05-2021 19:40"), 120);
-            boxOffice.AddInShow(new Performance("Король забавляется", "Виктор Гюго", "Драма", 1832), new Hall("Главный зал", 10, 10), Convert.ToDateTime("13-05-2021 20:40"), 200);
+            boxOffice.AddInShow(new Performance("Тартюф", "Мольер", "Комедия", 1664), new Hall("Главный зал", 1, 10), Convert.ToDateTime("15-05-2021 19:40"), 100);
+            boxOffice.AddInShow(new Performance("Тартюф", "Мольер", "Комедия", 1664), new Hall("Главный зал", 15, 15), Convert.ToDateTime("15-05-2021 18:40"), 150);
+            boxOffice.AddInShow(new Performance("Укрощение строптивой", "Уильям Шекспир", "Комедия", 1954), new Hall("Главный зал", 10, 10), Convert.ToDateTime("15-05-2021 20:40"), 120);
+            boxOffice.AddInShow(new Performance("Укрощение строптивой", "Уильям Шекспир", "Комедия", 1954), new Hall("Главный зал", 10, 10), Convert.ToDateTime("15-05-2021 19:40"), 120);
+            boxOffice.AddInShow(new Performance("Король забавляется", "Виктор Гюго", "Драма", 1832), new Hall("Главный зал", 10, 10), Convert.ToDateTime("16-05-2021 20:40"), 200);
             UserInteraction.Menu(posterPerformances, boxOffice);
         }
     }
